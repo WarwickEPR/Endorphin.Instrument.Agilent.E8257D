7838c2e2a7c4027ef4131ad61be3b43fc1115db6
Colin Stephen <C.J.Stephen@warwick.ac.uk>
2016-05-16 17:16:24 +0100

    Further corrections for the Agilent rf source, mainly to support output of modulations to LO

---
+++ AgilentModulatedSweep.fsx
@@ -0,0 +1,47 @@
+﻿#r "../Endorphin.Core/bin/Debug/Endorphin.Core.dll"
+#r "../packages/log4net.2.0.5/lib/net45-full/log4net.dll"
+#r "bin/Debug/Endorphin.Instrument.Agilent.E8257D.dll"
+
+open Microsoft.FSharp.Data.UnitSystems.SI.UnitSymbols
+open Endorphin.Instrument.Agilent.E8257D
+
+log4net.Config.BasicConfigurator.Configure ()
+
+let dwell = 0.01<s>
+let frequencyA = 1.234e9<Hz>
+let frequencyB = 1.345e9<Hz>
+let points = 500
+
+let modulationFrequency = 2.0e4<Hz>
+let modulationFn = Modulation.Configure.internalSineSourceInHzWithLfOutput modulationFrequency 0.1<V>
+let modulationDepth = 60.0<Hz>
+let modulation = FrequencyModulation (FM1, { Deviation = Frequency_Hz modulationDepth }, modulationFn)
+
+let sweep = Sweep.Configure.frequencyStepSweepInHz frequencyA frequencyB
+            |> Sweep.Configure.withPoints points
+            |> Sweep.Configure.withFixedPowerInDbm -3.0<dBm>
+            |> Sweep.Configure.withDwellTime (Some (Duration_sec dwell))
+            |> Sweep.Configure.withStepTrigger (Some Bus)
+            |> Sweep.Configure.withRetrace Off
+            |> Sweep.StepSweep
+
+let settings = { Sweep = sweep; Modulation = [ modulation ] }
+
+try
+    async {
+        // open the keysight box - set the VISA access string you need here and timeout
+        let! rfSource = RfSource.openInstrument "TCPIP::192.168.0.3::INSTR" 2000<ms>
+        printfn "Opened instrument"
+
+        do! RfSource.applySettings rfSource settings
+        printfn "Applied settings"
+        for i in 1..20 do
+            printfn "Triggering"
+            do! Sweep.Runtime.busTrigger rfSource
+            do! Async.Sleep 8000
+
+        // tidy up and close
+        do! RfSource.closeInstrument rfSource}
+    |> Async.RunSynchronously
+with
+    | :? InstrumentErrorException as exn -> printfn "Failed with instrument errors: %A" exn.Data0
